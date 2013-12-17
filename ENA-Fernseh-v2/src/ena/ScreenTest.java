16fbe644642a710e409083828cb4c01606e9d022
xRobbe <Robin.Rodenhausen@gmail.com>
2013-12-17 23:14:47 +0100

    Workaround for bugged PiP window, still not fixed

--- ENA-Fernseh-v2/src/ena/ScreenTest.java
+++ ENA-Fernseh-v2/src/ena/ScreenTest.java
@@ -1,5 +1,6 @@
 package ena;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -52,6 +53,8 @@ public class ScreenTest {
 	private Thread playingThread;
 	private int timeshift;
 
+	private int pipChannel;
+
 	// public static void main(String[] args) {
 	// EventQueue.invokeLater(new Runnable() {
 	// public void run() {
@@ -84,9 +87,9 @@ public class ScreenTest {
 
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 0.2, 0.4921875, .1078125, 0.1921875, 0.0078125, Double.MIN_VALUE };
+		gridBagLayout.columnWeights = new double[] { 0.2, 0.4921875, 0.1078125, 0.1921875, 0.0078125 };
 		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0 };
-		gridBagLayout.rowWeights = new double[] { 0.0138, 0.3, 0.5083, 0.1632, 0.0138, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0138, 0.3, 0.5083, 0.1632, 0.0138 };
 
 		GridBagConstraints c = new GridBagConstraints();
 
@@ -114,7 +117,7 @@ public class ScreenTest {
 
 		// panelScreenPiP
 		panelScreenPiP = new JPanel();
-		panelScreenPiP.setLayout(new GridBagLayout());
+		panelScreenPiP.setLayout(null);
 		panelScreenPiP.setVisible(false);
 		c.fill = GridBagConstraints.BOTH;
 		c.weightx = 0.3;
@@ -127,14 +130,14 @@ public class ScreenTest {
 
 		// lblScreenPiPPicture
 		lblScreenPiPPicture = new JLabel();
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1.0;
-		c.weighty = 1.0;
-		c.gridx = 0;
-		c.gridy = 0;
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		panelScreenPiP.add(lblScreenPiPPicture, c);
+		// c.fill = GridBagConstraints.BOTH;
+		// c.weightx = 1.0;
+		// c.weighty = 1.0;
+		// c.gridx = 0;
+		// c.gridy = 0;
+		// c.gridwidth = 1;
+		// c.gridheight = 1;
+		panelScreenPiP.add(lblScreenPiPPicture);
 
 		// progressBarScreenTimeshift
 		progressBarScreenTimeshift = new JProgressBar();
@@ -199,6 +202,40 @@ public class ScreenTest {
 							panelScreenTV.add(lblScreenTvPicture, c);
 							panelScreenTV.revalidate();
 						}
+						if (panelScreenPiP.isVisible()) {
+//							BufferedImage img = resize(ImageIO.read(new File(remote.getChannel().getChannelList().get(pipChannel).getChannelPicturePath())),
+//									(int) (panelScreenTV.getWidth() * 0.3),
+//									(int) (panelScreenTV.getHeight() * 0.3));
+//							panelScreenPiP.remove(lblScreenPiPPicture);
+//							lblScreenPiPPicture = new JLabel(new ImageIcon(img));
+//
+//							// GridBagConstraints c = new GridBagConstraints();
+//							// c.fill = GridBagConstraints.BOTH;
+//							// c.weightx = 1.0;
+//							// c.weighty = 1.0;
+//							// c.gridx = 0;
+//							// c.gridy = 0;
+//							// c.gridwidth = 5;
+//							// c.gridheight = 4;
+//
+//							lblScreenPiPPicture.setBounds(0, 0, (int) (panelScreenTV.getWidth() * 0.3), (int) (panelScreenTV.getHeight() * 0.3));
+//							panelScreenPiP.add(lblScreenTvPicture);
+//							panelScreenTV.revalidate();
+							
+							BufferedImage img = resize(ImageIO.read(new File(remote.getChannel().getChannelList().get(pipChannel).getChannelPicturePath())), (int) (panelScreenTV.getWidth() * 0.3), (int) (panelScreenTV.getHeight() * 0.3));
+							panelScreenPiP.remove(lblScreenPiPPicture);
+							lblScreenPiPPicture = new JLabel(new ImageIcon(img));
+//							c.fill = GridBagConstraints.NONE;
+//							c.weightx = 1.0;
+//							c.weighty = 1.0;
+//							c.gridx = 0;
+//							c.gridy = 0;
+							lblScreenPiPPicture.setBounds(0, 0, (int) (panelScreenTV.getWidth() * 0.3), (int) (panelScreenTV.getHeight() * 0.3));
+							panelScreenPiP.add(lblScreenPiPPicture);
+							panelScreenPiP.setMaximumSize((new Dimension((int) (panelScreenTV.getWidth() * 0.3), (int) (panelScreenTV.getHeight() * 0.3))));
+							panelScreenTV.revalidate();
+							
+						}
 						Thread.sleep(300);
 					}
 				} catch (Exception e) {
@@ -262,13 +299,16 @@ public class ScreenTest {
 				img = resize(img, (int) (panelScreenTV.getWidth() * 0.3), (int) (panelScreenTV.getHeight() * 0.3));
 				panelScreenPiP.remove(lblScreenPiPPicture);
 				lblScreenPiPPicture = new JLabel(new ImageIcon(img));
-				c.fill = GridBagConstraints.BOTH;
-				c.weightx = 1.0;
-				c.weighty = 1.0;
-				c.gridx = 0;
-				c.gridy = 0;
-				panelScreenPiP.add(lblScreenPiPPicture, c);
-				panelScreenPiP.revalidate();
+//				c.fill = GridBagConstraints.NONE;
+//				c.weightx = 1.0;
+//				c.weighty = 1.0;
+//				c.gridx = 0;
+//				c.gridy = 0;
+				lblScreenPiPPicture.setBounds(0, 0, (int) (panelScreenTV.getWidth() * 0.3), (int) (panelScreenTV.getHeight() * 0.3));
+				panelScreenPiP.add(lblScreenPiPPicture);
+				panelScreenPiP.setMaximumSize((new Dimension((int) (panelScreenTV.getWidth() * 0.3), (int) (panelScreenTV.getHeight() * 0.3))));
+				panelScreenTV.revalidate();
+				this.pipChannel = remote.getSelectedChannel();
 			}
 			else {
 				panelScreenTV.remove(lblScreenTvPicture);
